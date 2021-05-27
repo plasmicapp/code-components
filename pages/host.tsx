da63f123050cf3c33a506ca57428b2a5c1028eeb
Victor Agnez Lima <victor.agnez46@gmail.com>
2021-05-27 13:32:17 -0300

    Clean up host.tsx

--- pages/host.tsx
+++ pages/host.tsx
@@ -15,156 +15,153 @@ import { GoogleMap } from "@react-google-maps/api";
 
 import Head from "next/head";
 
+registerComponent(Slider, {
+  name: "Slider",
+  props: {
+    disabled: "boolean",
+    range: "boolean",
+    vertical: "boolean",
+  },
+  importPath: "antd",
+});
+
+registerComponent(Menu, {
+  name: "Menu",
+  props: {
+    mode: "string",
+    theme: "string",
+    selectedKeys: "object",
+    defaultSelectedKeys: "object",
+    children: {
+      type: "slot",
+      allowedComponents: ["MenuItem"],
+    },
+  },
+  importPath: "antd",
+});
+
+registerComponent(MenuItem, {
+  name: "MenuItem",
+  props: {
+    key: "string",
+    children: "slot",
+  },
+  importPath: "antd/lib/menu/MenuItem",
+  isDefaultExport: true,
+});
+
+registerComponent(Collapse, {
+  name: "Collapse",
+  props: {
+    defaultActiveKey: "object",
+    children: {
+      type: "slot",
+      allowedComponents: ["CollapsePanel"],
+    },
+  },
+  importPath: "antd",
+});
+
+registerComponent(CollapsePanel, {
+  name: "CollapsePanel",
+  props: {
+    header: "string",
+    key: "string",
+    children: "slot",
+  },
+  importPath: "antd/lib/collapse/CollapsePanel",
+  isDefaultExport: true,
+});
+
+registerComponent(Alert, {
+  name: "Alert",
+  props: {
+    message: "string",
+    description: "slot",
+    type: "string",
+    showIcon: "boolean",
+  },
+  importPath: "antd",
+});
+
+registerComponent(YouTube, {
+  name: "YouTube",
+  props: {
+    videoId: "string",
+  },
+  importPath: "react-youtube",
+  isDefaultExport: true,
+});
+
+registerComponent(RadialChart, {
+  name: "Radial Chart",
+  props: {
+    data: "object",
+    height: "number",
+    width: "number",
+  },
+  importPath: "react-vis",
+  importName: "RadialChart",
+});
+
+registerComponent(XYPlot, {
+  name: "XY Plot",
+  props: {
+    height: "number",
+    width: "number",
+    xDomain: "object",
+    yDomain: "object",
+    children: {
+      type: "slot",
+      allowedComponents: ["X Axis", "Y Axis", "Bar Series"],
+    },
+  },
+  importPath: "react-vis",
+  importName: "XYPlot",
+});
+
+registerComponent(XAxis, {
+  name: "X Axis",
+  props: {},
+  importPath: "react-vis",
+  importName: "XAxis",
+});
+
+registerComponent(YAxis, {
+  name: "Y Axis",
+  props: {},
+  importPath: "react-vis",
+  importName: "YAxis",
+});
+
+registerComponent(VerticalBarSeries, {
+  name: "Bar Series",
+  props: {
+    data: "object",
+    barWidth: "number",
+  },
+  importPath: "react-vis",
+  importName: "VerticalBarSeries",
+});
+
+registerComponent(GoogleMap, {
+  name: "Map",
+  props: {
+    center: "object",
+    zoom: "number",
+  },
+  importPath: "@react-google-maps/api",
+  importName: "GoogleMap",
+  classNameProp: "mapContainerClassName",
+});
+
 function Host() {
-  const [canvasHost, setCanvasHost] = React.useState<React.ReactNode>(null);
-  React.useEffect(() => {
-    registerComponent(Slider, {
-      name: "Slider",
-      props: {
-        disabled: "boolean",
-        range: "boolean",
-        vertical: "boolean",
-      },
-      importPath: "antd",
-    });
-
-    registerComponent(Menu, {
-      name: "Menu",
-      props: {
-        mode: "string",
-        theme: "string",
-        selectedKeys: "object",
-        defaultSelectedKeys: "object",
-        children: {
-          type: "slot",
-          allowedComponents: ["MenuItem"],
-        },
-      },
-      importPath: "antd",
-    });
-
-    registerComponent(MenuItem, {
-      name: "MenuItem",
-      props: {
-        key: "string",
-        children: "slot",
-      },
-      importPath: "antd/lib/menu/MenuItem",
-      isDefaultExport: true,
-    });
-
-    registerComponent(Collapse, {
-      name: "Collapse",
-      props: {
-        defaultActiveKey: "object",
-        children: {
-          type: "slot",
-          allowedComponents: ["CollapsePanel"],
-        },
-      },
-      importPath: "antd",
-    });
-
-    registerComponent(CollapsePanel, {
-      name: "CollapsePanel",
-      props: {
-        header: "string",
-        key: "string",
-        children: "slot",
-      },
-      importPath: "antd/lib/collapse/CollapsePanel",
-      isDefaultExport: true,
-    });
-
-    registerComponent(Alert, {
-      name: "Alert",
-      props: {
-        message: "string",
-        description: "slot",
-        type: "string",
-        showIcon: "boolean",
-      },
-      importPath: "antd",
-    });
-
-    registerComponent(YouTube, {
-      name: "YouTube",
-      props: {
-        videoId: "string",
-      },
-      importPath: "react-youtube",
-      isDefaultExport: true,
-    });
-
-    registerComponent(RadialChart, {
-      name: "Radial Chart",
-      props: {
-        data: "object",
-        height: "number",
-        width: "number",
-      },
-      importPath: "react-vis",
-      importName: "RadialChart",
-    });
-
-    registerComponent(XYPlot, {
-      name: "XY Plot",
-      props: {
-        height: "number",
-        width: "number",
-        xDomain: "object",
-        yDomain: "object",
-        children: {
-          type: "slot",
-          allowedComponents: ["X Axis", "Y Axis", "Bar Series"],
-        },
-      },
-      importPath: "react-vis",
-      importName: "XYPlot",
-    });
-
-    registerComponent(XAxis, {
-      name: "X Axis",
-      props: {},
-      importPath: "react-vis",
-      importName: "XAxis",
-    });
-
-    registerComponent(YAxis, {
-      name: "Y Axis",
-      props: {},
-      importPath: "react-vis",
-      importName: "YAxis",
-    });
-
-    registerComponent(VerticalBarSeries, {
-      name: "Bar Series",
-      props: {
-        data: "object",
-        barWidth: "number",
-      },
-      importPath: "react-vis",
-      importName: "VerticalBarSeries",
-    });
-
-    registerComponent(GoogleMap, {
-      name: "Map",
-      props: {
-        center: "object",
-        zoom: "number",
-      },
-      importPath: "@react-google-maps/api",
-      importName: "GoogleMap",
-      classNameProp: "mapContainerClassName",
-    });
-    setCanvasHost(<PlasmicCanvasHost />);
-  }, []);
   return (
     <div>
       <Head>
         <script src="https://studio.plasmic.app/static/js/preamble.js"/>
       </Head>
-      {canvasHost}
+      <PlasmicCanvasHost />
     </div>
   );
 }
