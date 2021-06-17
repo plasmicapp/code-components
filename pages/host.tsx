4db2a2757bf5c9ce49b21483c8d9a34c2a01d026
Yang Zhang <yang@users.noreply.github.com>
2021-06-16 17:09:30 -0700

    Use the documented script preamble

--- pages/host.tsx
+++ pages/host.tsx
@@ -359,7 +359,14 @@ function Host() {
   return (
     <div>
       <Head>
-        <script src="https://studio.plasmic.app/static/js/preamble.js" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+        !function(){const n=window,i="__REACT_DEVTOOLS_GLOBAL_HOOK__",o="__PlasmicPreambleVersion",t=function(){}
+if(void 0!==n){if(n.parent!==n)try{n[i]=n.parent[i]}catch(e){}if(!n[i]){const r=new Map
+n[i]={supportsFiber:!0,renderers:r,inject:function(n){r.set(r.size+1,n)},onCommitFiberRoot:t,onCommitFiberUnmount:t}}n[i][o]||(n[i][o]="1")}}()`,
+          }}
+        ></script>
         <link
           href="https://fonts.googleapis.com/css?family=Material+Icons&display=block"
           rel="stylesheet"
