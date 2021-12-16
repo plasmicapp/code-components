f4b277a20987b8aafe2fb0cc4ffc2b8a9fac0813
Samuel <samuel@plasmic.app>
2021-12-15 21:07:56 -0300

    Fix parallax scroll bug
    In some cases, the react-scroll-parallax wasn't working as expected.

--- components/ParallaxWrapper.tsx
+++ components/ParallaxWrapper.tsx
@@ -1,6 +1,31 @@
-import { ComponentProps, useContext } from "react";
-import { Parallax } from "react-scroll-parallax";
+import React, { ComponentProps, useContext } from "react";
+import { Parallax, ParallaxProvider, useController } from "react-scroll-parallax";
 import { PlasmicCanvasContext } from "@plasmicapp/loader-nextjs";
+
+function ParallaxCacheUpdate(props: React.PropsWithChildren<{}>) {
+  const parallaxController = useController();
+  
+  React.useEffect(() => {
+    const targetNode = document.body;
+    const observer = new MutationObserver(() => {
+      if (parallaxController) {
+        parallaxController.update();
+      }
+    });
+    observer.observe(targetNode, {childList: true, subtree: true});
+  });
+
+  return null;
+}
+
+export function ParallaxProviderWrapper(props: React.PropsWithChildren<{}>) {
+  return (
+    <ParallaxProvider>
+      <ParallaxCacheUpdate />
+      {props.children}  
+    </ParallaxProvider>
+  )
+}
 
 export function ParallaxWrapper(props: ComponentProps<typeof Parallax>) {
   const inEditor = useContext(PlasmicCanvasContext);
